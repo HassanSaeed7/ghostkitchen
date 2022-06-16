ca52a42470a3d9e3d68d2802d10147258dcfdcdd
Hassan <hsaeed1007@gmail.com>
2022-06-15 22:19:23 -0700

    cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -64,6 +64,11 @@ export default function Nav() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link href='/cart'
+                  className="p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                >
+                  <Image src='/cart.svg' alt='Shopping Cart' width={45} height={30} />
+                </Link>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
