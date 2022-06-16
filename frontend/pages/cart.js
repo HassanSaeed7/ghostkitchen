d27f938e0ebf37c0e2c107209a932ae03a2c95b7
Hassan <hsaeed1007@gmail.com>
2022-06-15 22:11:11 -0700

    cart

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,14 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 const cartItems = [
-  {"id": "1", "name": "CBD Gummies", "price": 30, "qty": 1},
-  {"id": "2", "name": "CBD Tincture", "price": 40, "qty": 1},
+  {"id": "1", "name": "CBD Gummies", "price": 30, "qty": 1, "image": "/featured0.jpg"},
+  {"id": "2", "name": "CBD Tincture", "price": 40, "qty": 1, "image": "/oil.jpg"},
 ]
 
 const Cart = () => {
 
 
-  const getSum = (total, num) => { 
+  const getSum = (total, num) => { cbd
 
     return total + num 
   }
@@ -19,12 +20,20 @@ const Cart = () => {
 
         <div>
           
-          <ul className='max-w-screen-lg m-auto divide-y divide-stone-300'>
-            {cartItems.map(({ id, name, price }) => ( <li key={id} className='py-10 flex justify-between'> <p>{name}</p> <p>${price}</p></li>) )}  
+          <ul className='m-auto max-w-xs divide-y divide-stone-300 xl:max-w-screen-lg lg:max-w-screen-md md:max-w-screen-sm'>
+            {cartItems.map(({ id, name, price, image }) => ( 
+            <li key={id} className='py-10 flex justify-between items-center'> 
+              <div className='flex items-center gap-2'> 
+              <Image src={image} alt={name} width={100} height={125}/> 
+              <p>{name}</p> 
+              </div>
+              
+              <p>${price}</p>
+            </li>) )}  
           </ul>
         </div>
 
-        <div className='max-w-screen-lg flex flex-col items-end gap-5'>
+        <div className='flex flex-col items-center gap-5 lg:items-end xl:max-w-screen-lg lg:max-w-screen-md md:max-w-screen-sm'>
           <p className='subtotal'>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</p>
           <p className='w-18'>Subtotal: ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}</p>
           <button className="w-18 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
