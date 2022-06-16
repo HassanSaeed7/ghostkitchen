7ff166735287ae767df64f6b8260ee0f171cb9d1
Hassan <hsaeed1007@gmail.com>
2022-06-15 21:04:29 -0700

    cart

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Cart = () => {
   return (
     <div>
-        cart
+        <h1>Cart</h1>
+
+        <div>
+          //map the added to cart items
+        </div>
+
+        <button>
+          Checkout
+        </button>
     </div>
   )
 }
