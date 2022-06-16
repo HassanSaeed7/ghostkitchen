9c676756b35f235e29ae06e1836f7a25c22a48d6
Hassan <hsaeed1007@gmail.com>
2022-06-16 03:10:59 -0700

    faq

--- frontend/pages/faq.js
+++ frontend/pages/faq.js
@@ -4,10 +4,10 @@ const faq = () => {
   return (
     <div className='min-h-screen flex justify-center items-center'>
 
-<div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item border-t-0 border-l-0 border-r-0 rounded-none bg-white border border-gray-200">
-    <h2 class="accordion-header mb-0" id="flush-headingOne">
-      <button class="accordion-button
+<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-xs" >
+  <div className="accordion-item border-t-0 border-l-0 border-r-0 rounded-none bg-white border border-gray-200">
+    <h2 className="accordion-header mb-0" id="flush-headingOne">
+      <button className="accordion-button
     relative
     flex
     items-center
@@ -17,23 +17,23 @@ const faq = () => {
     text-base text-gray-800 text-left
     bg-white
     border-0
-    rounded-none
-    transition
-    focus:outline-none" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+    focus:outline-none" type="button"
         aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
+        What is CBD?
       </button>
     </h2>
-    <div id="flush-collapseOne" class="accordion-collapse border-0 collapse show"
+    <div id="flush-collapseOne" className="accordion-collapse border-0 collapse show"
       aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body py-4 px-5">Placeholder content for this accordion, which is intended to
-        demonstrate
-        the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+      <div className="accordion-body py-4 px-5">
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercise nulla pariatur et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercise nulla pariatur et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+        
+        </div>
     </div>
   </div>
-  <div class="accordion-item border-l-0 border-r-0 rounded-none bg-white border border-gray-200">
-    <h2 class="accordion-header mb-0" id="flush-headingTwo">
-      <button class="accordion-button
+  <div className="accordion-item border-l-0 border-r-0 rounded-none bg-white border border-gray-200">
+    <h2 className="accordion-header mb-0" id="flush-headingTwo">
+      <button className="accordion-button
     collapsed
     relative
     flex
@@ -51,18 +51,18 @@ const faq = () => {
         Accordion Item #2
       </button>
     </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse border-0 collapse" aria-labelledby="flush-headingTwo"
+    <div id="flush-collapseTwo" className="accordion-collapse border-0 collapse" aria-labelledby="flush-headingTwo"
       data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body py-4 px-5">Placeholder content for this accordion, which is intended to
+      <div className="accordion-body py-4 px-5">Placeholder content for this accordion, which is intended to
         demonstrate
-        the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this
+        the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this
         being
         filled with some actual content.</div>
     </div>
   </div>
-  <div class="accordion-item border-l-0 border-r-0 border-b-0 rounded-none bg-white border border-gray-200">
-    <h2 class="accordion-header mb-0" id="flush-headingThree">
-      <button class="accordion-button
+  <div className="accordion-item border-l-0 border-r-0 border-b-0 rounded-none bg-white border border-gray-200">
+    <h2 className="accordion-header mb-0" id="flush-headingThree">
+      <button className="accordion-button
     collapsed
     relative
     flex
@@ -80,11 +80,11 @@ const faq = () => {
         Accordion Item #3
       </button>
     </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
+    <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree"
       data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body py-4 px-5">Placeholder content for this accordion, which is intended to
+      <div className="accordion-body py-4 px-5">Placeholder content for this accordion, which is intended to
         demonstrate
-        the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting
+        the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting
         happening here in terms of content, but just filling up the space to make it look, at least at first
         glance,
         a bit more representative of how this would look in a real-world application.</div>
