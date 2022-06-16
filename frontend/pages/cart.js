02883862bda2f2d3d71e4c4d4392b140046869b6
Hassan <hsaeed1007@gmail.com>
2022-06-15 21:15:16 -0700

    cart

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+const cartItems = [
+  {"id": "1", "name": "CBD Gummies", "price": "30", },
+  {"id": "2", "name": "CBD Tincture", "price": "40", }
+]
+
 const Cart = () => {
   return (
-    <div>
-        <h1>Cart</h1>
+    <div className='min-h-screen m-auto divide-y divide-stone-600 xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm'>
+        <h1 className='text-center text-5xl'>Cart</h1>
 
         <div>
-          //map the added to cart items
+          
+          <ul className='flex flex-col justify-center align-center'>
+            {cartItems.map(({ id, name, price }) => ( <li key={id}>{name} {price}</li>) )}  
+          </ul>
         </div>
 
         <button>
