0ed0308c897f2b6df402535c03438e6d2444989a
Hassan <hsaeed1007@gmail.com>
2022-08-04 13:33:27 -0700

    footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -4,31 +4,32 @@ import Image from "next/image";
 import Logo from "../public/samo.png";
 import Link from "next/link";
 
-const footerNav = [
+const navigation = [
   { name: "Facebook", src: "/facebook.svg", href: "#" },
   { name: "Instagram", src: "/instagram.svg", href: "#" },
   { name: "Email", src: "/email.svg", href: "#" },
   { name: "Phone", src: "/phone.svg", href: "#" },
-];
-
-const navigation = [
   { name: "Home", href: "/" },
   { name: "Order", href: "/order" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
+  { name: "Legal", href: "#" },
+  { name: "Terms", href: "#" },
+  { name: "Catering", href: "#" },
+  { name: "Sanitation", href: "#" },
 ];
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white min-h-twothirds">
-      <div className="min-h-twothirds grid lg:grid-cols-3 items-center gap-20 m-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+    <div className="bg-black text-white min-h-[60vh]">
+      <div className="w-full min-h-half flex justify-center items-center gap-20 m-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
         <div className="flex flex-col justify-center items-center">
           <div className="text-center">
             <Image
               src={Logo}
               alt="logo"
-              width={100}
-              height={100}
+              width={80}
+              height={80}
               className="m-auto fill-white"
             />
             <h6 className="text-center text-4xl font-bold mb-5">
@@ -39,32 +40,25 @@ const Footer = () => {
           <Hours />
         </div>
 
-        <div className="flex justify-center items-center lg:items-start">
-          <ul className="w-full flex flex-col gap-5">
-            {footerNav.map((i) => (
-              <li className="h-16 rounded-lg border border-white">
-                <a
-                  href={i.href}
-                  className="pl-[25%] flex items-center h-full text-lg gap-3"
-                >
-                  <Image src={i.src} alt={i.name} width={64} height={64} />
-                  <p>{i.name}</p>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="flex gap-4 justify-center items-center lg:items-start">
-          <ul className="w-full flex flex-col gap-5">
+        <div className="flex justify-start items-center lg:items-start">
+          <ul className="w-full grid grid-rows-4 grid-cols-3 grid-flow-col justify-evenly gap-5">
             {navigation.map((i) => (
-              <li className="h-16 w-full border border-white rounded-lg ">
-                <Link href={i.href}>
-                <a className="flex justify-center items-center h-full text-lg ">
+            <li className="h-16 w-full rounded-lg hover:bg-indigo-600 transition-colors">
+
+              {i.src
+              ? <Link href={i.href}>
+                <a className="p-5 flex items-center h-full text-lg gap-3">
+                <Image src={i.src} alt={i.name} width={30} height={30} />
+                <p>{i.name}</p>
+              </a>
+              </Link>
+              : <Link href={i.href}>
+                <a className="p-5 flex justify-center items-center h-full text-lg ">
                   {i.name}
                 </a>
-                </Link>
-              </li>
+                </Link>}
+            </li>
+
             ))}
           </ul>
         </div>
--- frontend/components/Hours.js
+++ frontend/components/Hours.js
@@ -3,18 +3,17 @@ import React from 'react'
 const Hours = () => {
   return (
     <>
-        <h3 className='text-xl italic text-center'>Hours of Operation:</h3>
         <ul className='w-full grid items-center gap-2'>
             <li className='flex justify-between px-4 lg:px-0'>
-                <span className='text-xl font-bold'>Monday - Wednesday</span>
+                <span className='text-lg italic'>Mon - Wed</span>
                 <span>12 pm - 12 am</span>
             </li>
             <li className='flex justify-between px-4 lg:px-0'>
-                <span className='text-xl font-bold'>Thursday - Saturday</span>
+                <span className='text-lg italic'>Thurs - Sat</span>
                 <span>12 pm - 2 am</span>
             </li>
             <li className='flex justify-between px-4 lg:px-0'>
-                <span className='text-xl font-bold'>Sunday</span>
+                <span className='text-lg italic'>Sun</span>
                 <span>Closed</span>
             </li>
         </ul>
